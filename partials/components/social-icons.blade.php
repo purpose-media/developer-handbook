20238e5ce232712aff2b2adf5319ca7f2df2cd42
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2017-09-13 14:32:44 +0100

    added social icon component

---
+++ partials/components/social-icons.blade.php
@@ -0,0 +1,8 @@
+<ul class="social">
+    <li class="social__label">Some Label</li>
+    {!! social_icon( 'facebook', '<li class="social__link"><a target="social" href="%s" class="facebook" title="facebook"><i class="fa fa-facebook-official"></i></a></li>' ) !!}
+    {!! social_icon( 'twitter', '<li class="social__link"><a target="social" href="%s" class="twitter" title="twitter"><i class="fa fa-twitter-square"></i></a></li>' ) !!}
+    {!! social_icon( 'instagram', '<li class="social__link"><a target="social" href="%s" class="instagram" title="instagram"><i class="fa fa-instagram"></i></a></li>' ) !!}
+    {!! social_icon( 'youtube', '<li class="social__link"><a target="social" href="%s" class="youtube" title="youtube"><i class="fa fa-youtube-play"></i></a></li>' ) !!}
+    {!! social_icon( 'google_plus', '<li class="social__link"><a target="social" href="%s" class="google" title="google+"><i class="fa fa-google-plus-square"></i></a></li>' ) !!}
+</ul>
